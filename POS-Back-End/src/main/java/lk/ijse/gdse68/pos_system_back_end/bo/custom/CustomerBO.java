1b55007cea1e844986e023b94fcba30c9b3d16c9
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-08-02 20:22:46 +0530

    create customer bo,boImpl

---
+++ POS-Back-End/src/main/java/lk/ijse/gdse68/pos_system_back_end/bo/custom/CustomerBO.java
@@ -0,0 +1,20 @@
+package lk.ijse.gdse68.pos_system_back_end.bo.custom;
+
+import lk.ijse.gdse68.pos_system_back_end.bo.SuperBO;
+import lk.ijse.gdse68.pos_system_back_end.dto.CustomerDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface CustomerBO extends SuperBO {
+    boolean saveCustomer(Connection connection, CustomerDTO customerDTO) throws SQLException;
+
+    ArrayList<CustomerDTO> getAllCustomers(Connection connection) throws SQLException;
+
+    CustomerDTO getCustomerById(Connection connection, String id) throws SQLException;
+
+    boolean updateCustomer(Connection connection, CustomerDTO customerDTO) throws SQLException;
+
+    boolean deleteCustomer(Connection connection, String id) throws SQLException;
+}
