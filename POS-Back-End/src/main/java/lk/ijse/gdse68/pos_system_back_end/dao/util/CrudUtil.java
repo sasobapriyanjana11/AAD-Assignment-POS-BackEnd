a3db188ed987cb6648aa9a2ac52bee0bf82e57c2
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-08-02 20:19:30 +0530

    add web.xml

---
+++ POS-Back-End/src/main/java/lk/ijse/gdse68/pos_system_back_end/dao/util/CrudUtil.java
@@ -0,0 +1,21 @@
+package lk.ijse.gdse68.pos_system_back_end.dao.util;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CrudUtil {
+    public static <T> T execute (Connection connection, String sql, Object... args) throws SQLException{
+        PreparedStatement pstm = connection.prepareStatement(sql); // DbConnection class is that not using ,using to web app web-inf is this connected to db connection
+
+        for (int i=0; i<args.length; i++){
+            pstm.setObject((i+1),args[i]);
+        }
+
+        if (sql.startsWith("SELECT") || sql.startsWith("select")){
+            return (T) pstm.executeQuery(); // result set ek
+        }
+
+        return (T) (Boolean) (pstm.executeUpdate() > 0);
+    }
+}
