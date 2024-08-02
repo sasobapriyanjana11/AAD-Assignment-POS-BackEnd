25b8fb83f432184973c4559a9525a6d3fdf568e9
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-08-02 20:22:00 +0530

    create customer entity

---
+++ POS-Back-End/src/main/java/lk/ijse/gdse68/pos_system_back_end/entity/Customer.java
@@ -0,0 +1,18 @@
+package lk.ijse.gdse68.pos_system_back_end.entity;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Setter
+@Getter
+@ToString
+public class Customer {
+    private String id;
+    private String name;
+    private String address;
+    private Double salary;
+
+
+}
