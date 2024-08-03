39c4b76a42e7700f5d32decf141729da5de42ee0
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-08-03 21:26:57 +0530

    create item dao class

--- POS-Back-End/src/main/java/lk/ijse/gdse68/pos_system_back_end/dao/custom/impl/ItemDAOImpl.java
+++ POS-Back-End/src/main/java/lk/ijse/gdse68/pos_system_back_end/dao/custom/impl/ItemDAOImpl.java
@@ -1,35 +1,74 @@
 package lk.ijse.gdse68.pos_system_back_end.dao.custom.impl;
 
 import lk.ijse.gdse68.pos_system_back_end.dao.custom.ItemDAO;
+import lk.ijse.gdse68.pos_system_back_end.dao.util.CrudUtil;
+import lk.ijse.gdse68.pos_system_back_end.entity.Customer;
 import lk.ijse.gdse68.pos_system_back_end.entity.Item;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ItemDAOImpl implements ItemDAO {
+    private String code;
+    private String name;
+    private BigDecimal price;
+
+    private int qty;
     @Override
     public boolean save(Connection connection, Item entity) throws SQLException {
-        return false;
+        String sql = "INSERT INTO item (code,name,qty,price) VALUES (?,?,?,?,?)";
+        return CrudUtil.execute(connection,sql,entity.getCode(),entity.getName(),entity.getQty(),entity.getPrice());
     }
 
     @Override
     public boolean update(Connection connection, Item entity) throws SQLException {
-        return false;
+        String sql = "UPDATE item SET name = ?,qty = ?, price = ? WHERE code = ?";
+        return CrudUtil.execute(connection,sql,entity.getName(),entity.getQty(),entity.getPrice(),entity.getCode());
+
     }
 
     @Override
     public ArrayList<Item> getAll(Connection connection) throws SQLException {
-        return null;
+        String sql = "SELECT * FROM item";
+        ArrayList<Item> itemArrayList = new ArrayList<Item>();
+        ResultSet rst = CrudUtil.execute(connection, sql);
+
+        while(rst.next()){
+            Item item = new Item(
+                    rst.getString(1),
+                    rst.getString(2),
+                    rst.getInt(4),
+                    rst.getBigDecimal(3)
+
+
+            );
+
+            itemArrayList.add(item);
+        }
+        return itemArrayList;
     }
 
     @Override
-    public boolean delete(Connection connection, String id) throws SQLException {
-        return false;
+    public boolean delete(Connection connection, String code) throws SQLException {
+        String sql = "DELETE FROM item WHERE code=?";
+        return CrudUtil.execute(connection,sql,code);
     }
 
     @Override
-    public Item findBy(Connection connection, String id) throws SQLException {
-        return null;
+    public Item findBy(Connection connection, String code) throws SQLException {
+        String sql = "SELECT * FROM item WHERE code=?";
+        Item item =new Item();
+        ResultSet rst = CrudUtil.execute(connection,sql,code);
+
+        if (rst.next()){
+            item.setCode(rst.getString(1));
+            item.setName(rst.getString(2));
+            item.setQty(Integer.parseInt(rst.getString(3)));
+            item.setPrice(rst.getBigDecimal(4));
+        }
+        return item;
     }
 }
