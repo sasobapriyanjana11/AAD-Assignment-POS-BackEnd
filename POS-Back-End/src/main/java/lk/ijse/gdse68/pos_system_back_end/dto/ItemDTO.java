317cdacb79ea5b2a04ed98021fb9ee7f49832790
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-08-03 21:26:14 +0530

    create item DTO

--- POS-Back-End/src/main/java/lk/ijse/gdse68/pos_system_back_end/dto/ItemDTO.java
+++ POS-Back-End/src/main/java/lk/ijse/gdse68/pos_system_back_end/dto/ItemDTO.java
@@ -1,11 +1,19 @@
 package lk.ijse.gdse68.pos_system_back_end.dto;
 
-import java.math.BigDecimal;
+import lombok.*;
 
+import java.math.BigDecimal;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Setter
+@Getter
+@ToString
 public class ItemDTO {
     private String code;
     private String name;
-    private BigDecimal price;
-    private String status;
     private int qty;
+    private BigDecimal price;
+
+
 }
