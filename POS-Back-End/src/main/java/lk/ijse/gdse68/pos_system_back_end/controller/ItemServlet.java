9dba27c0c51e3ecaa94de611082b8087ed0e2a46
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-08-03 22:40:43 +0530

    create ItemServlet and implementation

--- POS-Back-End/src/main/java/lk/ijse/gdse68/pos_system_back_end/controller/ItemServlet.java
+++ POS-Back-End/src/main/java/lk/ijse/gdse68/pos_system_back_end/controller/ItemServlet.java
@@ -1,4 +1,177 @@
 package lk.ijse.gdse68.pos_system_back_end.controller;
 
-public class ItemServlet {
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.json.bind.JsonbException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.gdse68.pos_system_back_end.bo.custom.ItemBO;
+import lk.ijse.gdse68.pos_system_back_end.bo.custom.impl.ItemBOImpl;
+import lk.ijse.gdse68.pos_system_back_end.dto.ItemDTO;
+import lombok.var;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.ArrayList;
+
+@WebServlet(name = "item" ,urlPatterns = "/item" ,loadOnStartup = 3)
+public class ItemServlet extends HttpServlet {
+
+
+    ItemBO itemBO=new ItemBOImpl();
+    DataSource connectionPool;
+
+
+    @Override
+    public void init() throws ServletException {
+        try {
+            var ctx = new InitialContext(); //get connection to connection pool
+            Context envContext = (Context) ctx.lookup("java:/comp/env");
+            DataSource dataSource = (DataSource) envContext.lookup("jdbc/pos_system_new");
+            this.connectionPool = dataSource;
+        } catch (NamingException e) {
+            throw new ServletException("Cannot find JNDI resource", e);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (Connection connection = connectionPool.getConnection()) {
+            Jsonb jsonb = JsonbBuilder.create();
+
+            ItemDTO itemDTO = jsonb.fromJson(req.getReader(), ItemDTO.class);
+            System.out.println(itemDTO);
+
+            if (itemDTO.getCode() == null || !itemDTO.getCode().matches("^(I00-)[0-9]{3}$")) {
+                resp.getWriter().write("Item id is empty or invalid!!");
+                return;
+            } else if (itemDTO.getName() == null || !itemDTO.getName().matches("^[A-Za-z ]{4,}$")) {
+                resp.getWriter().write("Name is empty or invalid!!");
+                return;
+            } else if (itemDTO.getQty() <=0) {
+                resp.getWriter().write("Quantity is empty !!");
+                return;
+//            } else if (itemDTO.getPrice() <= 0) {
+//                resp.getWriter().write("price is invalid!!");
+//                return;
+            }
+
+            boolean isSaved = itemBO.saveItem(connection, itemDTO);
+            if (isSaved) {
+                resp.setStatus(HttpServletResponse.SC_CREATED);
+            } else {
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "failed to save item");
+            }
+        } catch (SQLIntegrityConstraintViolationException e) {
+            resp.sendError(HttpServletResponse.SC_CONFLICT, "Duplicate values. Please check again");
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "An error occurred while processing the request.");
+        }
+    }
+
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String function = req.getParameter("function");
+
+        if (function.equals("getAll")) {
+            try (Connection connection = connectionPool.getConnection()) {
+                ArrayList<ItemDTO> itemDTOList = itemBO.getAllItems(connection);
+
+                Jsonb jsonb = JsonbBuilder.create();
+                String json = jsonb.toJson(itemDTOList);
+                resp.getWriter().write(json);
+            } catch (JsonbException e) {
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+            } catch (IOException e) {
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+            } catch (SQLException e) {
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+            }
+        } else if (function.equals("getById")) {
+            String code = req.getParameter("code");
+            try (Connection connection = connectionPool.getConnection()) {
+                ItemDTO itemDTO = itemBO.getItemById(connection,code);
+
+                Jsonb jsonb = JsonbBuilder.create();
+                String json = jsonb.toJson(itemDTO);
+                resp.getWriter().write(json);
+            } catch (JsonbException e) {
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+            } catch (IOException e) {
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+            } catch (SQLException e) {
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+            }
+        }
+    }
+
+
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (Connection connection = connectionPool.getConnection()) {
+            Jsonb jsonb = JsonbBuilder.create();
+               ItemDTO itemDTO = jsonb.fromJson(req.getReader(), ItemDTO.class);
+              System.out.println(itemDTO);
+
+            if (itemDTO.getCode() == null || !itemDTO.getCode().matches("^(I00-)[0-9]{3}$")) {
+                resp.getWriter().write("item id is empty or invalid!");
+                return;
+            } else if (itemDTO.getName() == null || !itemDTO.getName().matches("^[A-Za-z ]{4,}$")) {
+                resp.getWriter().write("Name is empty or invalid! ");
+                return;
+            } else if (itemDTO.getQty() <=0) {
+                resp.getWriter().write("item qty is empty or invalid");
+                return;
+//            } else if (customerDTO.getSalary() <= 0) {
+//                resp.getWriter().write("Salary is empty or invalid!!");
+//                return;
+
+            }
+            boolean isUpdated = itemBO.updateItem(connection, itemDTO);
+            if (isUpdated) {
+                resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
+
+            } else {
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "failed to update item");
+            }
+
+
+        } catch (SQLIntegrityConstraintViolationException e) {
+            resp.sendError(HttpServletResponse.SC_CONFLICT, "Duplicate values. Please check again");
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "An error occurred while processing the request.");
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String code = req.getParameter("code");
+        try (Connection connection = connectionPool.getConnection()){
+            boolean isDeleted = itemBO.deleteItem(connection,code);
+            if (isDeleted){
+                resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            }else{
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,"Failed to delete item!");
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
 }
